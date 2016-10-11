2bbea1c48561a373aac7ed9850531b79cdd5db7f
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-11 14:30:47 -0500

    "Configured index.js with passport, mongoose, and express"

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+    , config = require("./server/config")
+    , app = express()
+    , port = process.env.PORT || 3000;
+
+// JSON and Session
+const {json} = require("body-parser")
+    , session = require("express-session");
+app.use(json());
+app.use(express.static(`${__dirname}/public`));
+app.use(session({secret: config.session.secret}));
+
+// Passport
+const passport = require("passport")
+    , strategy = require("./server/config/facebookStrategy");
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+
+// Mongoose
+const mongoose = require("mongoose");
+mongoose.connect(config.database.mongoUri);
+mongoose.connection.once("open", () => console.log(`Connected to MongoDB at ${config.database.mongoUri}`));
+
+//Routes
+require("./server/masterRoutes")(app);
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
