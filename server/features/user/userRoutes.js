ad821c757882f25a6166cd1d7fb3a858da235ae7
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-11 14:46:28 -0500

    "Added facebook auth endpoints to userRoutes"

---
+++ server/features/user/userRoutes.js
@@ -0,0 +1,10 @@
+const userCtrl = require("./userCtrl")
+    , passport = require("passport")
+    , auth = require("../../middleware/auth");
+
+module.exports = app => {
+  app.get("/auth/facebook", passport.authenticate("facebook"));
+  app.get("/auth/facebook/callback",
+    passport.authenticate("facebook", {successRedirect: "http://localhost:3000/#/user", failureRedirect: "/login"})
+  );
+}
