a371987982efc9fe465792f1344dadc82ad4a1d9
Brian Hudson <brianjasonhudson@gmail.com>
2016-10-11 15:13:52 -0500

    "Configured app.js with home and login states"

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+angular.module("languageApp", ["ui.router", "ui.materialize"])
+.config(function($urlRouterProvider, $stateProvider) {
+  $urlRouterProvider.otherwise("shop");
+  $stateProvider
+    .state("home", {
+    controller: mainController
+    , url: "/"
+    , template: homeHtml
+  })
+
+  .state("login", {
+    controller: userController
+    , url: "/login"
+    , template: loginHtml
+  })
+
+  .state("user", {
+    controller: userController
+    , url: "/user"
+    , template: userHtml
+  })
+
+  $urlRouterProvider.otherwise("/");
+  } );
